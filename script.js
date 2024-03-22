36d51bf15aed3799f5680a012a56161aacf09aee
Darm02 <david02rangel@gmail.com>
2024-03-21 23:26:09 -0300

    distancia

--- script.js
+++ script.js
@@ -29,6 +29,7 @@ function createFlower() {
             // Verificar si la nueva posición está lo suficientemente alejada de las posiciones existentes
             for (const position of existingPositions) {
                 const distance = Math.sqrt(Math.pow(position.x - randomX, 2) + Math.pow(position.y - randomY, 2));
+                distance = distance == 0 ? 10 : distance;
                 if (distance < 0) { // Rango de 300 píxeles recomendado para pc, en celular con 0
                     positionValid = false;
                     break;
